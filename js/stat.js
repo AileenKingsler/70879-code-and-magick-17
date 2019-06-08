e8b94be360b7d2371111370046d36923165911fb
Aileen <aileen.kingsler@mail.ru>
2019-06-08 12:34:29 +1000

    Выносит код в отдельные функции

--- js/stat.js
+++ js/stat.js
@@ -13,6 +13,7 @@ var FONT_GAP = 20;
 var contentX = CLOUD_X + CLOUD_PADDING;
 var contentY = CLOUD_Y + CLOUD_PADDING;
 var histogramY = contentY + FONT_GAP * 2 + 5;
+var nameY = histogramY + FONT_GAP + HISTOGRAM_HEIGHT + 5;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -31,6 +32,28 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getRandomSaturate = function () {
+  return 'saturate(' + Math.random() * 100 + '%)';
+};
+
+var drawHistogram = function (ctx, name, x, y, width, height) {
+  if (name === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    ctx.filter = 'none';
+  } else {
+    ctx.fillStyle = 'rgba(0, 0, 255, 1)';
+    ctx.filter = getRandomSaturate();
+  }
+
+  ctx.fillRect(x, y, width, height);
+};
+
+var drawText = function (ctx, name, time, left, nameTop, timeTop) {
+  ctx.fillStyle = '#000';
+  ctx.fillText(name, left, nameTop);
+  ctx.fillText(Math.round(time), left, timeTop);
+};
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -45,17 +68,11 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     var columnHeight = (HISTOGRAM_HEIGHT * times[i]) / maxTime;
+    var timeY = histogramY + HISTOGRAM_HEIGHT - columnHeight;
+    var columnX = contentX + (COLUMN_WIDTH + COLUMN_GAP) * i;
+    var columnY = timeY + FONT_GAP;
 
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-      ctx.filter = 'saturate(' + Math.random() + ')';
-    }
-
-    ctx.fillRect(contentX + (COLUMN_WIDTH + COLUMN_GAP) * i, histogramY + FONT_GAP + HISTOGRAM_HEIGHT - columnHeight, COLUMN_WIDTH, columnHeight);
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], contentX + (COLUMN_WIDTH + COLUMN_GAP) * i, histogramY + FONT_GAP + HISTOGRAM_HEIGHT + 5);
-    ctx.fillText(Math.round(times[i]), contentX + (COLUMN_WIDTH + COLUMN_GAP) * i, histogramY + HISTOGRAM_HEIGHT - columnHeight);
+    drawHistogram(ctx, players[i], columnX, columnY, COLUMN_WIDTH, columnHeight);
+    drawText(ctx, players[i], times[i], columnX, nameY, timeY);
   }
 };
